d7b2ef1315451ec4d82c327572012d24f027e764
uiandwe <uiandwe@gmail.com>
2016-07-06 02:17:24 +0900

    tutorial 12

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 
 let mixin = InnerComponent => class extends React.Component{
-	constructor(){
-		super();
-		this.update = this.update.bind(this);
-		this.state = {val: 0}
-	}
-	update(){
-		this.setState({val: this.state.val + 1})
-	}
-	componentWillMount(){
-		console.log('will')
-	}
+    constructor(){
+        super();
+        this.update = this.update.bind(this);
+        this.state = {val: 0}
+    }
+    update(){
+        this.setState({val: this.state.val + 1})
+    }
+    componentWillMount(){
+        console.log('will')
+    }
 
-	render(){
-		return <InnerComponent 
-		update = {this.update}
+    render(){
+        return <InnerComponent
+            update = {this.update}
 		{...this.state}
 		{...this.props} />
-	}
-	componentDidMount(){
-		console.log('mounted')
-	}
+    }
+    componentDidMount(){
+        console.log('mounted')
+    }
 
 }
 
@@ -31,16 +31,16 @@ const Button = (props) => <button onClick={props.update}> {props.txt}-{props.val
 let ButtonMixed  = mixin(Button)
 class App extends React.Component{
 
-	
-	render(){
-		return(
-			<div>
-				<ButtonMixed txt="Button" />
-			</div>
-		);
-	}
 
-	
+    render(){
+        return(
+            <div>
+                <ButtonMixed txt="Button" />
+            </div>
+        );
+    }
+
+
 }
 
 App.defaultProps = {txt : 'button'}
